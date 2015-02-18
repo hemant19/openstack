f4b68fe6e981455bac4fab65cfe1e6462e0b6665
Ashwini <ashwinisharnagat1@gmail.com>
2015-02-18 22:33:45 +0530

    Added horizon and nova

---
+++ horizon.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "You need to be 'root' dude." 1>&2
+   exit 1
+fi
+
+# get horizon
+apt-get install -y apache2 memcached libapache2-mod-wsgi openstack-dashboard
+
+# remove the ubuntu theme - seriously this is fucking stupid it's still broken
+apt-get remove -y --purge openstack-dashboard-ubuntu-theme
+
+# restart apache
+service apache2 restart; service memcached restart
+
+# source the setup and stack files
+. ./setuprc
+managementip=$MY_CONTROLLER_IP
+password=$MY_PASSWORD
+
+# patch nova.conf files - TODO
+
+echo "#######################################################################################"
+echo;
+echo "The horizon dashboard should be at http://$managementip/horizon.  Login with admin/$password"
+echo;
+echo "#######################################################################################"
+
